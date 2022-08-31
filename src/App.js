bba83885fa30bd04632dfc4b8e50d7c75c437745
Gonzalo Soriano <gonzalo@MacBook-Air-de-Gonzalo.local>
2022-08-30 23:50:56 -0500

    Estilo simplificado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <NavBar />
         <ItemListContainer greeting="¡Disfruta la bebida perfecta para cada ocasión!"/>
       </header>
